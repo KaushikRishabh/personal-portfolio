77e99cc9e579b10d2ec9698114c3a5092faea468
KaushikRishabh <reachmeatrish@gmail.com>
2023-11-19 22:30:22 +0530

    email changes

--- app/actions.ts
+++ app/actions.ts
@@ -5,6 +5,26 @@ import { Resend } from "resend";
 import ContactFormEmail from "@/email/contact-form-email";
 import { createElement } from "react";
 
+// debugger;
+// console.log(process.env.RESEND_API_KEY);
+
+// This is a simple example; you'll need to adjust it to fit your actual email content requirements
+function renderEmailContent(
+  senderEmail: string,
+  title: string,
+  message: string
+) {
+  return `
+    <html>
+      <body>
+        <h1>You received the following message from the contact form</h1>
+        <h2>Title: ${title}</h2>
+        <p>Detail: ${message}</p>
+        <p>The sender's email is: ${senderEmail}</p>
+      </body>
+    </html>
+  `;
+}
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(formData: FormData) {
@@ -27,6 +47,13 @@ export async function sendEmail(formData: FormData) {
       error: "Invalid Message",
     };
   }
+  // In your sendEmail function
+  const emailHtml = renderEmailContent(
+    senderEmail as string,
+    title as string,
+    message as string
+  );
+
   let data;
   try {
     data = await resend.emails.send({
@@ -34,11 +61,12 @@ export async function sendEmail(formData: FormData) {
       to: "reachmeatrish@gmail.com",
       subject: "Message from Contact Form",
       reply_to: senderEmail as string,
-      react: createElement(ContactFormEmail, {
-        message: message as string,
-        title: title as string,
-        senderEmail: senderEmail as string,
-      }),
+      react: emailHtml,
+      // createElement(ContactFormEmail, {
+      //   message: message as string,
+      //   title: title as string,
+      //   senderEmail: senderEmail as string,
+      // }),
     });
   } catch (error: unknown) {
     return {
